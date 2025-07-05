2d6e66da941f880f4a21aa604cc2918690f553aa
maharshijani05 <maharshijani1231@gmail.com>
2025-07-05 13:50:52 +0530

    Handle two comma-separated numbers

--- string_calculator_tdd/calculator.py
+++ string_calculator_tdd/calculator.py
@@ -1,4 +1,5 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-    return int(numbers)
+    num_list = map(int, numbers.split(","))
+    return sum(num_list)
--- string_calculator_tdd/test_calculator.py
+++ string_calculator_tdd/test_calculator.py
@@ -6,3 +6,6 @@ def test_empty_string_returns_zero():
 def test_single_number():
     assert add("10") == 10
 
+def test_two_numbers():
+    assert add("1,2") == 3
+
